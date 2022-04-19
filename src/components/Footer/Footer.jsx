f45f0b4a4af3e78770ecb2b6246284d115408910
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-19 11:46:50 -0500

    url links in footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './Footer.css';
-
-// This is one of our simplest components
-// It doesn't have local state, so it can be a function component.
-// It doesn't dispatch any redux actions or display any part of redux state
-// or even care what the redux state is, so it doesn't need 'connect()'
+import { HomeOutlined, ChangeHistory, Add, FormatListBulleted } from '@mui/icons-material';
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  const history = useHistory();
+  
+  return (
+    <footer>
+      <HomeOutlined onClick={() => { history.push('/home') }}/>
+      <ChangeHistory onClick={() => { history.push('/hierarchy') }}/>
+      <Add onClick={() => { history.push('/exposure-form') }}/>
+      <FormatListBulleted onClick={() => { history.push('/history') }}/> 
+    </footer>
+  )
 }
 
 export default Footer;
