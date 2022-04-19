21dd9b7f6d1c6417c8c752c4f6744a5f186f78bd
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-19 12:03:01 -0500

    lunch break'

--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Grid, Paper, Typography } from '@mui/material';
 
 function History() {
     const dispatch = useDispatch();
@@ -11,7 +12,20 @@ function History() {
     console.log('exposureList from store:', exposureList);
     
     return (
-        <p>History Component</p>
+        <>
+        <h3>HISTORY</h3>
+        <Grid container>
+            {exposureList.map(exposure => (
+                <Grid item key={exposure.id}>
+                    <Paper>
+                        <Typography>
+                            DATE: {exposure.date}
+                        </Typography>
+                    </Paper>
+                </Grid>  
+            ))}
+        </Grid>
+        </>
     )
 }
 
