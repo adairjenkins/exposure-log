b2b7335314220a76a47fb50bca3e24f4bec99dad
Maggie Jenkins <adair.jenkins@gmail.com>
2022-04-28 13:46:54 -0500

    about to branch

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {useSelector} from 'react-redux';
-import { Button } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { Button, Typography, Box } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 
 function LoginForm() {
@@ -29,42 +29,44 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
-      {errors.loginMessage && (
-        <h3 className="alert" role="alert">
-          {errors.loginMessage}
-        </h3>
-      )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <Button type="submit">LOG IN</Button>
-      {/* <div>
+    <Box sx={{margin:3}}>
+      <form className="formPanel" onSubmit={login}>
+        <Typography variant="h5">Login</Typography>
+        {errors.loginMessage && (
+          <h3 className="alert" role="alert">
+            {errors.loginMessage}
+          </h3>
+        )}
+        <Box sx={{ mt: 3 }}>
+          <label htmlFor="username">
+            Username:
+            <input
+              type="text"
+              name="username"
+              required
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </label>
+        </Box>
+        <Box sx={{mt:2}}>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              name="password"
+              required
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
+        </Box>
+        <Button variant="contained" type="submit" sx={{mt:3}}>SUBMIT</Button>
+        {/* <div>
         <input className="btn" type="submit" name="submit" value="Log In" />
       </div> */}
-    </form>
+      </form>
+    </Box>
   );
 }
 
