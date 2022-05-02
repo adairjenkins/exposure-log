2b9eab59e854aae63561635ad1ec51641ed5421d
Maggie Jenkins <adair.jenkins@gmail.com>
2022-05-02 09:11:49 -0500

    change paper color on hierarchy

--- src/components/Hierarchy/HierarchyList.jsx
+++ src/components/Hierarchy/HierarchyList.jsx
@@ -11,28 +11,50 @@ function HierarchyList() {
     const history = useHistory();
     const hierarchyList = useSelector(store => store.hierarchy);
     console.log('hierarchyList from store:', hierarchyList);
+    const exposureList = useSelector(store => store.exposure);
+    console.log('exposureList:', exposureList);
 
     useEffect(() => {
-        dispatch({ type: 'GET_HIERARCHY' })
+        dispatch({ type: 'GET_HIERARCHY' });
+        dispatch({ type: 'GET_EXPOSURE' });
     }, []);
 
     return (
         <>
-            <Stack spacing={1.5} sx={{ width: "90%", marginBottom: "70px", marginTop: "20px", marginLeft:2 }}>
+            <Stack spacing={1.5} sx={{ width: "90%", marginBottom: "70px", marginTop: "20px", marginLeft: 2 }}>
                 {hierarchyList.map(situation => (
-                    <Paper variant="outlined" key={situation.id} sx={{
-                        '&:hover': {
-                            backgroundColor: "#ededed",
-                        }, padding:1
-                    }}>
-                        {/* main view */}
-                        < Situation
-                            situation={situation}
-                        />
-                        {/* < EditSituation
+                    exposureList.some(exposure => exposure.hierarchy_id == situation.id) ?
+                        <>
+                            <Paper variant="outlined" key={situation.id} sx={{ backgroundColor:"#E6EDF5",
+                                '&:hover': {
+                                    backgroundColor: "#ededed",
+                                }, padding: 1
+                            }}>
+                                {/* main view */}
+                                < Situation
+                                    situation={situation}
+                                />
+                                {/* < EditSituation
                             situation={situation} 
                         /> */}
-                    </Paper>
+                            </Paper>
+                        </>
+                        :
+                        <>
+                            <Paper variant="outlined" key={situation.id} sx={{
+                                '&:hover': {
+                                    backgroundColor: "#ededed",
+                                }, padding: 1
+                            }}>
+                                {/* main view */}
+                                < Situation
+                                    situation={situation}
+                                />
+                                {/* < EditSituation
+                            situation={situation} 
+                        /> */}
+                            </Paper>
+                        </>
                 ))}
             </Stack>
             {/* <TableContainer component={Paper}>
